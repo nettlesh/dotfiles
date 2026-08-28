e390a6b4a5e9049185b6eef6c17d8d0059fac20d
Sebastian Battle <dev@sebastianbattle.com>
2026-08-27 21:36:03 -0400

    feat: switch to ghostty terminal, starship prompt, configure tldr, bat, and update agent instruction

--- fish/config.fish
+++ fish/config.fish
@@ -1,158 +1,57 @@
-# Man pages through bat
-set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+# Env vars
+set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc # ripgrep has no default config location
+set -gx TEALDEER_CONFIG_DIR ~/.config/tealdeer
+
+set -gx LESS -c # Display text from the top-down
+set -gx MANPAGER "sh -c 'col -bx | bat -plman'" # Use bat for man pages
 set -gx MANROFFOPT -c
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
-# ripgrep has no default config location
-set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
+set -gx EZA_ICONS_AUTO 1
+set -gx FZF_CTRL_T_OPTS "--walker-skip .git,node_modules,.venv,__pycache__,.ruff_cache,.pytest_cache,target --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+# PATH
+if test (uname) = Darwin
+    fish_add_path /opt/homebrew/bin
+end
 
 fish_add_path ~/.local/bin
 
 status is-interactive; or exit
 
-set -g fish_greeting
+set -g fish_greeting # Remove greeting
 fish_vi_key_bindings
-
-# Settings for the done plugin
-set -U __done_min_cmd_duration 10000
-set -U __done_notification_urgency_level low
-
-# Functions
-
-function backup --argument filename
-    cp $filename $filename.bak
-end
-
-# Copy DIR1 DIR2
-function copy
-    if test (count $argv) = 2; and test -d "$argv[1]"
-        command cp -r (string trim --right --chars=/ $argv[1]) $argv[2]
-    else
-        command cp $argv
-    end
-end
-
-function history
-    builtin history --show-time='%F %T ' $argv
-end
+set -g fish_cursor_default block blink
 
 # Aliases
-
-# Re-read fish's initialization file
 alias reload 'source ~/.config/fish/config.fish'
-
-# Navigation
-alias .. 'cd ..'
-alias ... 'cd ../..'
-alias .... 'cd ../../..'
-alias ..... 'cd ../../../..'
-alias ...... 'cd ../../../../..'
-alias dot 'cd ~/.dotfiles'
 alias dotfiles 'cd ~/.dotfiles'
 
-# Listing, replacing ls with eza
-alias ls 'eza -al --color=always --group-directories-first --icons=always' # Preferred listing
-alias la 'eza -a --color=always --group-directories-first --icons=always' # All files and dirs
-alias ll 'eza -l --color=always --group-directories-first --icons=always' # Long format
-alias lt 'eza -aT --color=always --group-directories-first --icons=always' # Tree listing
-alias l. "eza -al --ignore-glob '[!.]*'" # Dotfiles only
-alias tree 'eza --tree'
+alias ls 'eza --group-directories-first'
+alias l 'eza -l --group-directories-first'
+alias la 'eza -a --group-directories-first'
+alias ll 'eza -al --group-directories-first'
+alias tree 'eza -T --group-directories-first'
 
-# Editing
+alias cat='bat --paging=never' # Use bat insteaed
+
 alias n nvim
 alias v nvim
 alias vi nvim
 alias vim nvim
 
-# Searching
-alias egrep 'egrep --color=auto'
-alias fgrep 'fgrep --color=auto'
-alias grep 'grep --color=auto'
-
-# Archives
-alias tarnow 'tar -acf'
-alias untar 'tar -zxvf'
-
-# Processes by memory
-# Functions rather than aliases, because alias appends $argv to the last stage of a pipe
-function psmem
-    ps auxf | sort -nr -k 4
-end
-
-function psmem10
-    psmem | head -10
-end
-
-# Paste to termbin
-alias tb 'nc termbin.com 9999'
-
-# GNU coreutils, absent on macOS
-if type -q dir
-    alias dir 'dir --color=auto'
-    alias vdir 'vdir --color=auto'
-end
-
-if type -q hwinfo
-    alias hw 'hwinfo --short' # Hardware info
-end
-
-if type -q journalctl
-    alias jctl 'journalctl -p 3 -xb' # Error messages from the current boot
-end
-
-if type -q wget
-    alias wget 'wget -c' # Continue partial downloads
-end
-
-if type -q pacman
-    alias fixpacman 'sudo rm /var/lib/pacman/db.lck'
-
-    # Installed packages by size
-    function big
-        expac -H M '%m\t%n' | sort -h | nl
-    end
-
-    # Remove orphaned packages, which pacman errors on when there are none
-    function cleanup
-        set -l orphans (pacman -Qtdq)
-        if test -n "$orphans"
-            sudo pacman -Rns $orphans
-        else
-            echo "No orphaned packages found"
-        end
-    end
-
-    # Count -git packages
-    function gitpkg
-        pacman -Q | grep -i '\-git' | wc -l
-    end
-
-    # Recently installed
-    function rip
-        expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl
-    end
-
-    # CachyOS ships its own mirror ranker
-    if type -q cachyos-rate-mirrors
-        alias mirror 'sudo cachyos-rate-mirrors'
-        alias update 'sudo cachyos-rate-mirrors && sudo pacman -Syu'
-    else
-        alias update 'sudo pacman -Syu'
-    end
-end
-
-# mise activates first because it puts fnox on PATH
-mise activate fish | source
-
+# CLIs
+mise activate fish | source # mise activates first
 fnox activate fish | source
-tv init fish | source
+fzf --fish | source
 zoxide init fish | source
 
-# Auto start herdr (last because exec replaces this shell and herdr needs mise to have activated)
-# Ported from the ArchWiki: https://wiki.archlinux.org/title/Tmux#Start_tmux_on_every_shell_login
-# NOTE: Evaluating whether named sessions are preferred over one shared session
-if type -q herdr; and { test (uname) != Linux; or set -q DISPLAY }; and not set -q HERDR_ENV
-    exec herdr
+# Prompt
+function starship_transient_prompt_func
+    starship module character
 end
+
+starship init fish | source
+enable_transience
